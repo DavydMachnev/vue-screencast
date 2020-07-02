a3164297084bf5f6f1a31b68029ab67e342e9697
user <user@users-MacBook-Pro.local>
2020-07-02 21:24:45 +0300

    Home & WatchVideo

---
+++ src/views/VideoWatch.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <app-video-player
+      class="video-player-box"
+      ref="video-player"
+      :options="playerOptions"
+    >
+    </app-video-player>
+    <h1>{{ video.name }}</h1>
+    <div v-html="video.description"></div>
+  </div>
+</template>
+
+<script>
+import 'video.js/dist/video-js.css';
+import { videoPlayer } from 'vue-video-player';
+
+export default {
+  name: 'VideoWatch',
+  components: {
+    appVideoPlayer: videoPlayer,
+  },
+  computed: {
+    video() {
+      return this.$store.state.videos.find((vid) => vid.id === this.$route.params.id);
+    },
+    playerOptions() {
+      return {
+        language: 'en',
+        playbackRates: [0.7, 1.0, 1.5, 2.0],
+        sources: [{
+          type: "video/mp4",
+          src: this.video.videoUrl
+        }],
+        poster: this.video.thumbnail
+      };
+    },
+  },
+};
+</script>
+
+<style>
+.video-player-box .video-js {
+  margin: auto;
+}
+</style>
